9121145e255778aa810f4629a370fb71bf7fd737
Pedro <pedrokehl@hotmail.com>
2019-06-30 19:11:57 -0300

    Update dependencies and fix typo on "console.error"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SubtitleService = require('./subtitle-service');
 const subtitleService = new SubtitleService();
 
-subtitleService.downloadAllFromDir('C:/Users/pedro/Videos/')
+subtitleService.downloadAllFromDir('D:/Videos/')
     .then(console.log)
-    .catch(console.err);
+    .catch(console.error);
